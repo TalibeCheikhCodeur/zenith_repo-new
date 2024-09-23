99fb4e250c647ffea5057ebce2756f0b2673a72f
BAMBA <inkbamba44@gmail.com>
2024-09-23 01:44:03 +0000

    Configure Cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,15 +31,16 @@ class Kernel extends HttpKernel
      * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        // 'web' => [
+        //     \App\Http\Middleware\EncryptCookies::class,
+        //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        //     \Illuminate\Session\Middleware\StartSession::class,
+        //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        //     \App\Http\Middleware\VerifyCsrfToken::class,
+        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        // ],
         'api' => [   
+        \App\Http\Middleware\ForceHttps::class,
         \Illuminate\Http\Middleware\HandleCors::class, // Ajoutez cette ligne
         // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // Si vous avez besoin de Sanctum
         \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
