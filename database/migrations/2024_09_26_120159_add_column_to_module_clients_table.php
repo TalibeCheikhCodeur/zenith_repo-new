e6a23d174fea2e20d6cde78eb0b13048c9f2adba
khaoussou <khaoussoudiallo7@gmail.com>
2024-09-26 12:13:00 +0000

    add new column to module_clients_table

---
+++ database/migrations/2024_09_26_120159_add_column_to_module_clients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('module_clients', function (Blueprint $table) {
+            $table->string("mode_aquisition")->nullable();
+            $table->dateTime("date_fin_validite")->nullable();
+            $table->string("code_octroye")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('module_clients', function (Blueprint $table) {
+            //
+        });
+    }
+};
