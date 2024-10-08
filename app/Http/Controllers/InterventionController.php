bcba3f7d640d8f773214dd70686b61fa40778b8a
khaoussou <khaoussoudiallo7@gmail.com>
2024-10-08 10:45:23 +0000

    add link

--- app/Http/Controllers/InterventionController.php
+++ app/Http/Controllers/InterventionController.php
@@ -142,14 +142,13 @@ class InterventionController extends Controller
     {
         $intervention = Intervention::findOrFail($interventionId);
         $user = User::where('id', $userId)->first();
-        $mails = User::whereIn('role', ['DPT', 'DG'])->pluck('email'); 
+        $mails = User::whereIn('role', ['DPT', 'DG'])->pluck('email');
         // return $mails;
-        if (!$user)
-        {
-          return $this->response(Response::HTTP_OK, "L\'utilisateur n'existe pas", []);
+        if (!$user) {
+            return $this->response(Response::HTTP_OK, "L\'utilisateur n'existe pas", []);
         }
 
-        $this->sendMail([$user->email], "une intervention vous a été assigné");
+        $this->sendMail([$user->email], "une intervention vous a été assigné, vous pouvez vous connectez via ce lien: http://192.168.1.19:4200");
         $this->sendMail($mails, "une intervention a été assigné à $user->prenom");
 
         $intervention->user_id = $userId;
