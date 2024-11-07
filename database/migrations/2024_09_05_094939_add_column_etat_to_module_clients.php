0befe9d0bd08665a2aee348105fb4f9fc7f09299
BAMBA <inkbamba44@gmail.com>
2024-11-07 12:20:05 +0000

    updated

--- database/migrations/2024_09_05_094939_add_column_etat_to_module_clients.php
+++ database/migrations/2024_09_05_094939_add_column_etat_to_module_clients.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('module_clients', function (Blueprint $table) {
-            //
+            $table->dropColumn('etat');
         });
     }
 };
