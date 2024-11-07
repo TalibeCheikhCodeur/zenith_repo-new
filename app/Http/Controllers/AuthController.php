a9cac17be3c975bb1d1977789ddeb1d5d5b3d95a
BAMBA <inkbamba44@gmail.com>
2024-11-07 11:28:17 +0000

    updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         $user = Auth::user();
         $token = $user->createToken("token")->plainTextToken;
 
-        $expiresAt = Carbon::now()->addHours(24);  // Le Token va expirer sous 24 h
+        $expiresAt = Carbon::now()->addMinutes(2);  // Le Token va expirer sous 24 h
 
         $user->tokens()->create([
             'name' => 'auth_token',
